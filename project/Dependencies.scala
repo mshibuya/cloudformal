36787bf0dccb74840de45de640c14d960e4d3805
xuwei-k <6b656e6a69@gmail.com>
2018-04-03 19:38:09 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val argonaut = "io.argonaut" %% "argonaut" % "6.2"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+  lazy val argonaut = "io.argonaut" %% "argonaut" % "6.2.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val scopt = "com.github.scopt" %% "scopt" % "3.7.0"
 }
